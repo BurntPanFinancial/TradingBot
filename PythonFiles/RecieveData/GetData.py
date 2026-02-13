5d699b9c90d37ed6e45f830efae8a59985ad65c3
LucaMarks <lucamarks416@gmail.com>
2026-02-13 00:47:27 -0500

    updates ticker data live

--- PythonFiles/RecieveData/GetData.py
+++ PythonFiles/RecieveData/GetData.py
@@ -1,4 +1,6 @@
 # will be run in a virtual environment
+import os
+
 import yfinance as yf
 
 # storing name of file + intervalPeriod for each file
@@ -19,6 +21,7 @@ def getData(interval="5m", period="1d", namesOfStock=[]):
         )
         fileNames.append([namesOfStock[i], interval + " " + period])
         # print(fileNames[i])
+
         fileName = f"{fileNames[i][0]}_data.csv"
         df.to_csv(f"../TickerData/{fileName}")
 
@@ -29,17 +32,32 @@ def updateData(interval="5m", period="5m", namesOfStock=[]):
             tickers=namesOfStock[i], interval=interval, period=period, progress=False
         )
         fileNames.append([namesOfStock[i], interval + " " + period])
-        # print(fileNames[i])
-        fileName_buff = f"{fileNames[i][0]}_data_buffer.csv"
         # assume that the file already exists and we are updating it
-        df.to_csv(f"../TickerData/{fileName_buff}", mode="a", index=4, header=False)
-        # add the latest row from the buffer file to the main file
+
+        fileName_buff = f"{fileNames[i][0]}_data_buffer.csv"
         fileName = f"{fileNames[i][0]}_data.csv"
-        f = open(f"../TickerData/{fileName_buff}")
-        line = f.readline()
-        print(line)
 
+        df.to_csv(f"../TickerData/{fileName_buff}")
+        buff = open(f"../TickerData/{fileName_buff}", "r")
+        lineCount = 0
+        for line in buff:
+            lineCount += 1
 
-# add interval names to the end of fileNames
-# intervalPeriod = interval + " " + period
-# fileNames.append(intervalPeriod)
+        # reset the buffer file position
+        buff.seek(0)
+        for line in range(lineCount - 1):
+            buff.readline()
+
+        mainFile = open(f"../TickerData/{fileName}", "a")
+        # this is the last line in the buffer file, which would be the most recent
+        appendLine = buff.readline()
+        mainFile.write(appendLine)
+        buff.close()
+        mainFile.close()
+        print(f"added {appendLine} to {fileName}\n")
+        os.remove(f"../TickerData/{fileName_buff}")
+        # df.to_csv(f"../TickerData/{fileName}", mode="a", header=False)
+        # add the latest row from the buffer file to the main file
+        # this doesn't work because we are looking at the top, but appending would add to the bottom
+        # f = open(f"../TickerData/{fileName}")
+        # line = f.readline()
